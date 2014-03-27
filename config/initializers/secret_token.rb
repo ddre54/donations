549f67c0aef18424333e4b334fd946b32fa84dbb
David Rodas <ddreliv@gmail.com>
2014-03-26 22:46:25 -0400

    Initial commit of creation of donations application
    Basic template to start creating the donations application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Donations::Application.config.secret_key_base = 'ae1d60f010d6e982b495eb06565983a5bb477e3f9333770c3175d27974b4b2cf3c016e61be8c2c55e350d09963b0e6a05ace85aac0158b1068428b92ed9f64cf'
